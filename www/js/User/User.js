2b94b9af2b15746d11a303ed58123c4eceb0fce2
Peter Maatman <blackwolf12333@gmail.com>
2015-10-01 16:32:42 +0200

    Fix if statements

--- www/js/User/User.js
+++ www/js/User/User.js
@@ -24,20 +24,42 @@ angular.module('appcki.user',[])
             el.addClass(animation);
 
             UserService.signin(
+              {
+                  username: $scope.credentials.username,
+                  password: $scope.credentials.password
+              },
+              function(data){
+                  $location.path('/home');
+                  $scope.animating = false;
+              },
+              function(data){
+                $scope.animating = false;
+                if(data.status === 401)
                 {
-                    username: $scope.credentials.username,
-                    password: $scope.credentials.password
-                },
-                function(data){
-                    $location.path('/home');
-                    el.removeClass(animation);
-                },
-                function(){
-                    el.removeClass(animation);
-                    var alertPopup = $ionicPopup.alert({
-                       title: 'Inloggen mislukt',
-                       template: 'De gegevens kwamen niet overeen'
-                    });
+                  var alertPopup = $ionicPopup.alert({
+                    title: 'Inloggen mislukt',
+                    template: 'De gegevens kwamen niet overeen (response code 401)'
+                  }); 
+                } else if (data.status === 403) {
+                  var alertPopup = $ionicPopup.alert({
+                    title: 'Inloggen mislukt',
+                    template: 'De gegevens kwamen niet overeen. (Response code 403)'
+                  }); 
+                } else if (data.status === 500) {
+                  var alertPopup = $ionicPopup.alert({
+                    title: 'Inloggen mislukt',
+                    template: 'De server kon niet worden gevonden'
+                  }); 
+                } else if (data.status === 0) {
+                  var alertPopup = $ionicPopup.alert({
+                    title: 'Inloggen mislukt',
+                    template: 'We doen ons best, maar dit is echt apeshit. Heb je internet? Anders ligt het aan ons. Sorry. Response code 0'
+                  }); 
+                } else {
+                  var alertPopup = $ionicPopup.alert({
+                    title: 'Inloggen mislukt',
+                    template: 'Onbekende fout. Response code %s'.sprintf(data.status)
+                  }); 
                 }
             );
 		};
